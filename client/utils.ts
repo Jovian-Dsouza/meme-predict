85269fbc49e1279df7a6adbf6a8cafebbbb0c798
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-07-13 17:06:30 +0000

    feat: add test cases for initializing

---
+++ client/utils.ts
@@ -0,0 +1,37 @@
+import {  confirmTransaction } from "@solana-developers/helpers";
+import * as anchor from "@coral-xyz/anchor";
+import { web3, BN } from "@coral-xyz/anchor";
+
+export async function airdropSOL(provider, publicKey, amount) {
+  const signature = await provider.connection.requestAirdrop(publicKey, amount);
+  await confirmTransaction(provider.connection, signature);
+}
+
+export async function getBalance(
+  connection: web3.Connection,
+  publicKey: web3.PublicKey
+): Promise<number> {
+  const balance = await connection.getBalance(publicKey);
+  return balance / web3.LAMPORTS_PER_SOL;
+}
+
+// export async function confirmTransaction(
+//   connection: web3.Connection,
+//   txHash: string
+// ) {
+//   await connection.confirmTransaction(txHash);
+//   console.log(`TRANSACTION CONFIRMED -> ${txHash}`);
+// }
+
+export function toSol(amount: number): number {
+  return amount / web3.LAMPORTS_PER_SOL;
+}
+
+export function findCounterPDA(
+  programId: web3.PublicKey,
+): [web3.PublicKey, number] {
+  return anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("counter", "utf8")],
+    programId
+  );
+}
--- tests/meme-predict.ts
+++ tests/meme-predict.ts
@@ -1,16 +1,55 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { MemePredict } from "../target/types/meme_predict";
+import { findCounterPDA } from "../client/utils";
+import { SYSTEM_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/native/system";
+import { confirmTransaction } from "@solana-developers/helpers";
 
 describe("meme-predict", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.MemePredict as Program<MemePredict>;
+  const counterPDA = findCounterPDA(program.programId);
+  console.log("counterPDA", counterPDA.toString());
+
+  const coinPubKey = new anchor.web3.PublicKey(
+    "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263" //Bonk
+  );
+  const coinPrice = new anchor.BN(1000);
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods
+      .initializeCounter()
+      .accounts({
+        creator: provider.wallet.publicKey,
+      })
+      .rpc();
+
+    const counterData = await program.account.counter.fetch(counterPDA[0]);
+    console.log(counterData);
+    
+    const currentSlot = await program.provider.connection.getSlot();
+    const currentBlocktime = await program.provider.connection.getBlockTime(
+      currentSlot
+    );
+
+    await program.methods
+      .createMarket(
+        coinPubKey,
+        new anchor.BN(currentBlocktime + 60),
+        new anchor.BN(currentBlocktime + 120),
+        coinPrice
+      )
+      .accounts({
+        counter: counterPDA[0],
+        creator: provider.wallet.publicKey,
+      })
+      .rpc();;
+
+      const counterData2 = await program.account.counter.fetch(counterPDA[0]);
+      console.log(counterData2);
   });
 });
